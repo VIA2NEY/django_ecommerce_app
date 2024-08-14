93756169486ca36f4cb6de1e71fe1144c26b3b92
VIA2NEY <kouameangeV11@gmail.com>
2024-08-14 23:05:22 +0000

    createmodel, migration superadmin create

--- shop/products/admin.py
+++ shop/products/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import Product
+
 # Register your models here.
+# Rendre Product modifiable par l'admin
+admin.site.register(Product)
--- shop/products/models.py
+++ shop/products/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 
-# class Product(models.Model):
-#     name = models.CharField(max_length=100)
-#     price = models.DecimalField(max_digits=10, decimal_places=2)
-#     image = models.ImageField(upload_to='products/static/')
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='static/assets/Images/')
     
-#     def __str__(self):
-#         return self.name
+    def __str__(self):
+        return self.name
